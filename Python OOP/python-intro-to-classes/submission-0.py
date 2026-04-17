4deb79e5cb4ec4a62233caf90d3a427f3a268b6c
zehmbot <baichou88@gmail.com>
2026-04-17 11:46:04 +0100

    Add: python-intro-to-classes - submission-0

---
+++ Python OOP/python-intro-to-classes/submission-0.py
@@ -0,0 +1,12 @@
+
+class Pet():
+    def __init__(self, name: str, species: str):
+        self.name = name
+        self.species = species
+
+
+
+
+# Do not modify below this line
+my_pet = Pet("Fluffy", "cat")
+print(f"My pet is a {my_pet.species} named {my_pet.name}")
